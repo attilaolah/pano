b78eff3fe26687be523ec1089f98d7abc8f9cd73
Attila Oláh <attilaolah@gmail.com>
2023-10-18 00:49:37 +0200

    Hello, Triangle!

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ struct PanoViewer {
     // The window must be declared after the surface so it gets dropped after it as the surface
     // contains unsafe references to the window's resources.
     window: Window,
+
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl PanoViewer {
@@ -80,6 +82,51 @@ impl PanoViewer {
         };
         surface.configure(&device, &config);
 
+        let shader =
+            device.create_shader_module(wgpu::include_wgsl!("shaders/equirectangular.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         Self {
             window,
             surface,
@@ -87,6 +134,7 @@ impl PanoViewer {
             queue,
             config,
             size,
+            render_pipeline,
         }
     }
 
@@ -121,7 +169,7 @@ impl PanoViewer {
             });
 
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("render_pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -133,6 +181,8 @@ impl PanoViewer {
                 })],
                 depth_stencil_attachment: None,
             });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
